2545a1e985d6e4892c760942009599364b2824de
camilo3312 <ccvelasquez531@misena.edu.co>
2022-02-19 23:03:14 -0500

    first deploy

--- src/Components/Layout/Projects/Projects.jsx
+++ src/Components/Layout/Projects/Projects.jsx
@@ -2,40 +2,12 @@ import React, {useState, useEffect} from 'react'
 import { Card } from '../../Ui/Card/Card';
 import './Projects.css'
 
-const useCount = () => {
-    const [counter, setCounter] = useState(0)
-
-    const increment = (value) => setCounter(counter + value)
-    const decrement = (value) => setCounter(counter - value)
-    const reset = (value) => setCounter(value)
-    return {
-        counter,
-        increment,
-        decrement,
-        reset
-    }
-}
-
 export const Projects = () => {
-
-    const counterA = useCount()
-    const counterB = useCount()
-
-    useEffect(()=> {
-        console.log(counterB.counter);
-    },[counterB.counter])
-  
     return (
        <section>
            <div className='center_section_projects'>
-                <Card />
-                <button  onClick={()=>counterA.increment(3)}>+</button>
-                <button  onClick={()=>counterA.reset(0)}>rest</button>
-                <button  onClick={()=>counterA.decrement(10)}>-</button>
-           </div>
-                <button  onClick={()=>counterB.increment(3)}>+</button>
-                <button  onClick={()=>counterB.reset(0)}>rest</button>
-                <button  onClick={()=>counterB.decrement(10)}>-</button>
+                <Card /> 
+           </div> 
        </section>
     )
 };
